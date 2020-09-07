ee67e28649c8a2c4873795c8a818dd8b49423818
skubala <skubala.pawel@wp.pl>
2020-09-07 11:02:17 +0200

    Dodaj pliki projektów.

---
+++ Aplikacja_testowa/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Aplikacja_testowa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
